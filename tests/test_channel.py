6f9ea042b5a31e6c48026aeea8c86c1652f8072e
Krez7 <karimcool5900@gmail.com>
2026-04-21 22:52:12 +0200

    fix(tests): correct the paths used when calling pytest from the project's root

--- tests/test_channel.py
+++ tests/test_channel.py
@@ -3,6 +3,7 @@ import pytest
 import json
 from googleapiclient.discovery import build
 from googleapiclient.http import HttpMockSequence
+from pathlib import Path
 
 from ytb_analysis.channel import ChannelInstance
 
@@ -10,11 +11,12 @@ from ytb_analysis.channel import ChannelInstance
 class TestChannelInstance:
 
     def test_videos_list(self):
+        curr_dir = str(Path(__file__).parent)
         examples = []
         for i in range(4):
-            examples.append(({"status": "200"},json.dumps(json.load(open("responses/videos_list_" + str(i) + ".json")))))
+            examples.append(({"status": "200"},json.dumps(json.load(open(curr_dir + "/responses/videos_list_" + str(i) + ".json")))))
             
         http = HttpMockSequence(examples) 
         service = build("youtube", "v3", http=http)
         instance = ChannelInstance(service, "test")
-        assert instance.videos_list == json.load(open("responses/expected_videos_list.json")).get("items")
+        assert instance.videos_list == json.load(open(curr_dir + "/responses/expected_videos_list.json")).get("items")
