b1892b3d8edd9f35e976e34c705914ecfb81e8ea
krez7 <karimcool5900@gmail.com>
2026-07-10 22:20:57 +0200

    refactor(channel tests): use a single service for every test as a fixture
    Load all the mocked api responses before testing.

--- tests/test_channel.py
+++ tests/test_channel.py
@@ -7,16 +7,20 @@ from pathlib import Path
 
 from ytb_analysis.channel import ChannelInstance
 
+curr_dir = str(Path(__file__).parent)
 
 class TestChannelInstance:
 
-    def test_videos_list(self):
-        curr_dir = str(Path(__file__).parent)
+    @pytest.fixture
+    def instance(self):
         examples = []
+
         for i in range(4):
             examples.append(({"status": "200"},json.dumps(json.load(open(curr_dir + "/responses/videos_list_" + str(i) + ".json")))))
-            
-        http = HttpMockSequence(examples) 
+
+        http = HttpMockSequence(examples)
         service = build("youtube", "v3", http=http)
-        instance = ChannelInstance(service, "test")
+        return ChannelInstance(service, "test")
+
+    def test_videos_list(self, instance):
         assert instance.videos_list == json.load(open(curr_dir + "/responses/expected_videos_list.json")).get("items")
