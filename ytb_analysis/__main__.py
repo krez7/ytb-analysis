b23a6b4c9e53c4ff5b204186f3b9d928a3b83824
krez7 <karimcool5900@gmail.com>
2026-07-30 22:33:26 +0200

    feat: obtain OAuth 2.O authorization

--- ytb_analysis/__main__.py
+++ ytb_analysis/__main__.py
@@ -1,13 +1,24 @@
 from googleapiclient.discovery import build
-
+from google_auth_oauthlib.flow import InstalledAppFlow
 from .channel import ChannelInstance
 
 if __name__ == "__main__":
-    
-    api_key = input("Enter your Youtube Data API key : ")
+
+    api_key = ""
+    oauth_credentials = None
+
+    answer = input("Do you wish to provide OAuth access ? (y/n) : ")
+
+    if answer == "y":
+        oauth_secret_file = input("Enter your OAuth credientals file's name (must be in the root directory of the project) : ")
+        flow = InstalledAppFlow.from_client_secrets_file(oauth_secret_file, ["https://www.googleapis.com/auth/youtube.readonly", "https://www.googleapis.com/auth/yt-analytics.readonly"])
+        oauth_credentials = flow.run_local_server()
+    else:
+        api_key = input("Enter your Youtube Data API key : ")
+
     channel_id = input("Enter the channel's ID : ")
 
-    service = build("youtube", "v3", developerKey=api_key)
+    service = build("youtube", "v3", developerKey=api_key, credentials = oauth_credentials)
     
     instance = ChannelInstance(service, channel_id)
 
